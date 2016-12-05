989b585a1079448a7d9adaddf726739c119b4e58
inti0419 <inti0419@DESKTOP-A78J6C5>
2016-12-05 16:19:35 +0100

    Signed-off-by: inti0419 <inti0419@DESKTOP-A78J6C5>

--- GestionHotel/src/main/java/com/adaming/myapp/entities/Adresse.java
+++ GestionHotel/src/main/java/com/adaming/myapp/entities/Adresse.java
@@ -7,20 +7,19 @@
 
 package com.adaming.myapp.entities;
 
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Embeddable
 public class Adresse {
 
 	//=========================
 	// Attributes
 	//=========================
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idAdresse;
 	private String rue;
 	private int codePostal;
