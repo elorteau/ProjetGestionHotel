0cd0712673d5d65d897fdba03d5f857eb83458cf
inti0312 <inti0312@DESKTOP-43JK6QV>
2016-12-06 11:37:44 +0100

    Signed-off-by: inti0312 <inti0312@DESKTOP-43JK6QV>

--- GestionHotel/src/main/java/com/adaming/myapp/service/FactureServiceImpl.java
+++ GestionHotel/src/main/java/com/adaming/myapp/service/FactureServiceImpl.java
@@ -1,6 +1,10 @@
 package com.adaming.myapp.service;
 
+import java.util.List;
+
 import com.adaming.myapp.dao.IFactureDao;
+import com.adaming.myapp.entities.Facture;
+import com.adaming.myapp.exceptions.NullListException;
 
 public class FactureServiceImpl implements IFactureService{
 	
@@ -9,6 +13,39 @@ public class FactureServiceImpl implements IFactureService{
 	public void setDao(IFactureDao dao) {
 		this.dao = dao;
 	}
+
+	@Override
+	public Facture create(Facture f, Long idHotel) {
+		// TODO Auto-generated method stub
+		return dao.create(f, idHotel);
+	}
+
+	@Override
+	public Facture remplir(Facture f, Long idReservation, Long idConsommation) {
+		// TODO Auto-generated method stub
+		return dao.remplir(f, idReservation, idConsommation);
+	}
+
+	@Override
+	public Facture imprimer(Long idFacture) {
+		// TODO Auto-generated method stub
+		return dao.imprimer(idFacture);
+	}
+
+	@Override
+	public List<Facture> getAll() throws NullListException {
+		// TODO Auto-generated method stub
+		List<Facture> factures = dao.getAll();
+		if (factures.size()==0){
+			throw new NullListException("Aucune facture");}
+		else{
+			return dao.getAll();
+		}
+	}
+
+
+	
+	
 
 	
 }
--- GestionHotel/src/main/java/com/adaming/myapp/service/IFactureService.java
+++ GestionHotel/src/main/java/com/adaming/myapp/service/IFactureService.java
@@ -1,5 +1,15 @@
 package com.adaming.myapp.service;
 
+import java.util.List;
+
+import com.adaming.myapp.entities.Facture;
+import com.adaming.myapp.exceptions.NullListException;
+
 public interface IFactureService {
+	
+	public Facture create (Facture f, Long idHotel);
+	public Facture remplir (Facture f, Long idReservation, Long idConsommation);
+	public Facture imprimer(Long idFacture);
+	public List<Facture> getAll() throws NullListException;
 
 }
