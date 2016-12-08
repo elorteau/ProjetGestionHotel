52c62f4dad53a955ace1aca9e0e529ba6cd8974c
inti0419 <inti0419@DESKTOP-A78J6C5>
2016-12-08 11:29:12 +0100

    home avec le bootstrap la navbar dans un xhtml à part (include) et hotel.xhtml

--- GestionHotel/src/main/java/com/adaming/myapp/bean/HotelBean.java
+++ GestionHotel/src/main/java/com/adaming/myapp/bean/HotelBean.java
@@ -1,5 +1,28 @@
 package com.adaming.myapp.bean;
 
-public class HotelBean {
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
+
+import com.adaming.myapp.service.IHotelService;
+
+@Component("hotelBean")
+@ViewScoped
+public class HotelBean {
+	
+	private Logger LOGGER = Logger.getLogger("HotelBean");
+
+	@Inject
+	private IHotelService service;
+	
+	
+	public String forward(){
+		return "hotel";
+	}
+	
+	public String redirect(){
+		return "hotel?faces-redirect=true";
+	}
 }
